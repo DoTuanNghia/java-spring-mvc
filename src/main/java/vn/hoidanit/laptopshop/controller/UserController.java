7be31e0bb2892bbfebd0d2579c2a409de71a97ab
DoTuanNghia <dtn752k4@gmail.com>
2025-09-05 20:12:52 +0700

    update #50

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -1,20 +1,12 @@
 package vn.hoidanit.laptopshop.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import vn.hoidanit.laptopshop.service.UserService;
 
-//@Controller
-//public class UserController {
-//
-//    @RequestMapping("/")
-//    public String getHomePage() {
-//        return "hello from controller";
-//    }
-//}
-
-@RestController
+@Controller
 public class UserController {
 
     private UserService userService;
@@ -23,8 +15,26 @@ public class UserController {
         this.userService = userService;
     }
 
-    @RequestMapping("")
-    public String getHomePage() {
-        return this.userService.handleHello();
+    @RequestMapping("/")
+    public String getHomePage(Model model) {
+        String test = this.userService.handleHello();
+        model.addAttribute("eric", test);
+        model.addAttribute("hoidanit", "from controller with model");
+        return "hello";
     }
 }
+
+//@RestController
+//public class UserController {
+//
+//    private UserService userService;
+//
+//    public UserController(UserService userService) {
+//        this.userService = userService;
+//    }
+//
+//    @RequestMapping("")
+//    public String getHomePage() {
+//        return this.userService.handleHello();
+//    }
+//}
