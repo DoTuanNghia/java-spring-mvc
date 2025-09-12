697aec7909d5dc15032cb0edcf61449c070f8cf4
DoTuanNghia <dtn752k4@gmail.com>
2025-09-12 15:46:54 +0700

    #65 Design giao diện xem chi tiết User

--- src/main/java/vn/hoidanit/laptopshop/repository/UserRepository.java
+++ src/main/java/vn/hoidanit/laptopshop/repository/UserRepository.java
@@ -11,5 +11,5 @@ import java.util.List;
 public interface UserRepository extends JpaRepository<User, Long> {
     User save(User eric);
 
-    List<User> findByEmail(String email);
+    List<User> findOneByEmail(String email);
 }
